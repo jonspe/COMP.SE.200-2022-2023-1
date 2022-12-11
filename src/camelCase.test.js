04f28fcc382a52cb3e8dffbd1c21c6f9913be784
Joona Perasto <joona.perasto@tuni.fi>
2022-12-11 18:17:36 +0200

    Create tests for camelCase

--- src/camelCase.test.js
+++ src/camelCase.test.js
@@ -1,7 +1,47 @@
 import camelCase from "./camelCase.js"
 
-test("Convert the whole alphabet", () => {
-  expect(camelCase("The quick brown fox jumps over the lazy dog")).toBe(
-    "theQuickBrownFoxJumpsOverTheLazyDog"
-  )
+describe("Remove punctuation marks", () => {
+  it("should remove underscores", () => {
+    expect(camelCase("__Lorem__ipsum_dolor____sit___amet_")).toBe(
+      "loremIpsumDolorSitAmet"
+    )
+  })
+
+  it("should remove hyphens", () => {
+    expect(camelCase("--Lorem-ipsum---dolor--sit--------amet----")).toBe(
+      "loremIpsumDolorSitAmet"
+    )
+  })
+
+  it("should remove spaces", () => {
+    expect(camelCase("  Lorem  ipsum dolor  sit     amet  ")).toBe(
+      "loremIpsumDolorSitAmet"
+    )
+  })
+})
+
+describe("Test the alphabet", () => {
+  it("should convert the whole English alphabet", () => {
+    expect(camelCase("The quick brown fox jumps over the lazy dog")).toBe(
+      "theQuickBrownFoxJumpsOverTheLazyDog"
+    )
+  })
+
+  it("should convert umlauts", () => {
+    expect(camelCase("Äänekäs ääliö ölisee Zürichissa übach")).toBe(
+      "äänekäsÄäliöÖliseeZürichissaÜbach"
+    )
+  })
+
+  it("should handle numbers correctly", () => {
+    expect(camelCase("Lorem ipsum 483 dolor23sit7amet")).toBe(
+      "loremIpsum483Dolor23Sit7Amet"
+    )
+  })
+
+  it("should convert mixmatched capital letters correctly", () => {
+    expect(camelCase("LOreM iPsuM DoLOR siT amET")).toBe(
+      "loremIpsumDolorSitAmet"
+    )
+  })
 })
